d6072c9d7217aa8d73d779e8099d268ec7a1cfe7
Aldrin Jenson <aldrinjenson@gmail.com>
2023-01-21 23:44:10 +0530

    stream image directly instead of saving

--- src/pages/api/generateQR.js
+++ src/pages/api/generateQR.js
@@ -1,17 +1,9 @@
-const fs = require("fs");
 const QRCode = require("qrcode");
 const { createCanvas, loadImage } = require("canvas");
-
-function convertDataUriToImage(dataUri, filePath) {
-  const base64Data = dataUri.split(";base64,").pop();
-  const buffer = Buffer.from(base64Data, "base64");
-  fs.writeFileSync(filePath, buffer);
-}
 
 export default async function generateQR(req, res) {
   const center_image = "images/download.png";
   const { string } = req.body;
-  console.log(string);
 
   const width = 325; // To align the image in the center
   const cwidth = 75; // To increse the Quality of the image (Size)
@@ -19,21 +11,34 @@ export default async function generateQR(req, res) {
     const canvas = createCanvas(width, width);
     QRCode.toCanvas(canvas, string, {
       scale: 11,
-      // errorCorrectionLevel: 'L',
     });
 
     const ctx = canvas.getContext("2d");
     const img = await loadImage(center_image);
     const center = (width - cwidth) / 2 + 15;
     ctx.drawImage(img, center, center, cwidth, cwidth);
-    const filePath = "qrCodes/" + Math.random() + "qr.png";
 
-    convertDataUriToImage(canvas.toDataURL("image/png"), filePath);
-    res
-      .status(200)
-      .json({ message: "QR Code has been Successfully generated" });
+    const imageData = canvas.toDataURL("image/png").split(",")[1];
+    res.setHeader("Content-Type", "image/png");
+    res.send(Buffer.from(imageData, "base64"));
   } catch (error) {
     console.log(error);
     res.status(500).json({ error });
   }
 }
+
+// const filePath = "qrCodes/" + Math.random() + "qr.png";
+
+// const dataUri = canvas.toDataURL();
+// const base64Data = dataUri.split(";base64,").pop();
+// console.log(base64Data);
+
+// const buffer = Buffer.from(base64Data, "base64");
+// buffer.stre;
+// // return as resonse
+// fs.writeFileSync(filePath, buffer);
+
+// convertDataUriToImage(canvas.toDataURL("image/png"), filePath);
+// res
+//   .status(200)
+//   .json({ message: "QR Code has been Successfully generated" });
